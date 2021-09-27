c30a46f9bd2a801a5427fa83ed4edd8d486b739e
squteronsquter <squter@squter.pl>
2021-09-27 06:32:49 +0200

    templates for pages added

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Django Home Page')
+    return render(request, 'pages/index.html')
 
+def about(request):
+    return render(request, 'pages/about.html')
